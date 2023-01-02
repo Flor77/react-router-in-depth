2c489d5aba23650e7a98f9357398a8b9a1b3770d
Flor77 <budaflorin@yahoo.com>
2023-01-02 17:47:17 +0200

    error elements

--- src/pages/careers/CareerDetails.js
+++ src/pages/careers/CareerDetails.js
@@ -25,5 +25,9 @@ export const careerDetailsLoader = async ({ params }) => {
 
   const res = await fetch("http://localhost:4000/careers/" + id);
 
+  if (!res.ok) {
+    throw Error("Could not find that career.");
+  }
+
   return res.json();
 };
